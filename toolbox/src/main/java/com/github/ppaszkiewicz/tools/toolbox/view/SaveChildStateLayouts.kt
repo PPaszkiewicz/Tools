20c774643654dcced3bf637ab4ed7597c62b1944
PPaszkiewicz <pawel.paszkiewicz90@gmail.com>
2021-03-18 15:32:01 +0100

    refactor

--- toolbox/src/main/java/com/github/ppaszkiewicz/tools/toolbox/view/SaveChildStateLayouts.kt
+++ toolbox/src/main/java/com/github/ppaszkiewicz/tools/toolbox/view/SaveChildStateLayouts.kt
@@ -12,80 +12,89 @@ import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 
 /**
- * Helper holding implementation for storing/restoring save state for views children.
+ * Helper holding implementation for storing/restoring save state for views children, forcing parent to contain save of all children.
  *
- * To use it ensure all views children [View.isSaveFromParentEnabled] is disabled and they have set IDs.
+ * This will
+ * prevent save state collision due to duplicate view IDs within the same layout as long as parent containers have unique ID themselves.
  *
- * Then implement following overrides:
+ * Implement following overrides in your ViewGroup:
  *
- *     override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) = super.addView(child?.apply { isSaveFromParentEnabled = false }, index, params)
- *     override fun onSaveInstanceState() = ViewChildStateManager.onSaveInstanceState(this, super.onSaveInstanceState())
- *     override fun onRestoreInstanceState(state: Parcelable?) = super.onRestoreInstanceState(ViewChildStateManager.onRestoreInstanceState(this, state))
- *
+ *     override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>?) = ViewChildStateManager.dispatchSaveInstanceState(this, container, ::dispatchFreezeSelfOnly)
+ *     override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>?) = ViewChildStateManager.dispatchRestoreInstanceState(this, container, ::dispatchThawSelfOnly)
  * */
 object ViewChildStateManager {
     const val SAVE_SELF = "SAVE_STATE_SELF"
     const val SAVE_CHILDREN = "SAVE_STATE_CHILDREN"
 
     /** Merges [view] save state with its childrens save state.*/
-    fun onSaveInstanceState(view: ViewGroup, superState: Parcelable?): Parcelable? {
-        val b = Bundle()
-        b.putParcelable(SAVE_SELF, superState)
-        val sa = SparseArray<Parcelable>()
+    inline fun dispatchSaveInstanceState(
+        view: ViewGroup,
+        container: SparseArray<Parcelable>?,
+        dispatchFreezeSelfOnly: (SparseArray<Parcelable>?) -> Unit
+    ) {
+        if (!view.isSaveEnabled || view.id == FrameLayout.NO_ID) return
+        val selfState = SparseArray<Parcelable>(1)
+        dispatchFreezeSelfOnly(selfState)
+        val childrenState = SparseArray<Parcelable>()
         repeat(view.childCount) {
-            view.getChildAt(it).saveHierarchyState(sa)
+            view.getChildAt(it).let { v ->
+                if (v.isSaveFromParentEnabled) v.saveHierarchyState(childrenState)
+            }
         }
-        b.putSparseParcelableArray(SAVE_CHILDREN, sa)
-        return b
+        Bundle().apply {
+            putSparseParcelableArray(SAVE_SELF, selfState)
+            putSparseParcelableArray(SAVE_CHILDREN, childrenState)
+            container?.put(view.id, this)
+        }
     }
 
     /** Restores [view] children save state and returns this views state. */
-    fun onRestoreInstanceState(view: ViewGroup, state: Parcelable?): Parcelable? {
-        if (state == null) return null
-        val b = state as Bundle
-        val sa = b.getSparseParcelableArray<Parcelable>(SAVE_CHILDREN)
-        repeat(view.childCount) {
-            view.getChildAt(it).restoreHierarchyState(sa)
+    inline fun dispatchRestoreInstanceState(
+        view: ViewGroup,
+        container: SparseArray<Parcelable>?,
+        dispatchThawSelfOnly: (SparseArray<Parcelable>?) -> Unit
+    ) {
+        if (!view.isSaveEnabled || view.id == FrameLayout.NO_ID) return
+        (container?.get(view.id) as? Bundle)?.let { bundle ->
+            val selfState = bundle.getSparseParcelableArray<Parcelable>(SAVE_SELF)
+            val childrenState = bundle.getSparseParcelableArray<Parcelable>(SAVE_CHILDREN)
+            dispatchThawSelfOnly(selfState)
+
+            repeat(view.childCount) {
+                view.getChildAt(it).let { v ->
+                    if (v.isSaveFromParentEnabled) v.restoreHierarchyState(childrenState)
+                }
+            }
         }
-        return b.getParcelable(SAVE_SELF)
     }
 }
 
 open class SaveChildStateFrameLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
-    override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) =
-        super.addView(child?.apply { isSaveFromParentEnabled = false }, index, params)
+    override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>?) =
+        ViewChildStateManager.dispatchSaveInstanceState(this, container, ::dispatchFreezeSelfOnly)
 
-    override fun onSaveInstanceState() =
-        ViewChildStateManager.onSaveInstanceState(this, super.onSaveInstanceState())
-
-    override fun onRestoreInstanceState(state: Parcelable?) =
-        super.onRestoreInstanceState(ViewChildStateManager.onRestoreInstanceState(this, state))
+    override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>?) =
+        ViewChildStateManager.dispatchRestoreInstanceState(this, container, ::dispatchThawSelfOnly)
 }
 
 open class SaveChildStateConstraintLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-    override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) =
-        super.addView(child?.apply { isSaveFromParentEnabled = false }, index, params)
+    override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>?) =
+        ViewChildStateManager.dispatchSaveInstanceState(this, container, ::dispatchFreezeSelfOnly)
 
-    override fun onSaveInstanceState() =
-        ViewChildStateManager.onSaveInstanceState(this, super.onSaveInstanceState())
-
-    override fun onRestoreInstanceState(state: Parcelable?) =
-        super.onRestoreInstanceState(ViewChildStateManager.onRestoreInstanceState(this, state))
+    override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>?) =
+        ViewChildStateManager.dispatchRestoreInstanceState(this, container, ::dispatchThawSelfOnly)
 }
 
 open class SaveChildStateLinearLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) =
-        super.addView(child?.apply { isSaveFromParentEnabled = false }, index, params)
+    override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>?) =
+        ViewChildStateManager.dispatchSaveInstanceState(this, container, ::dispatchFreezeSelfOnly)
 
-    override fun onSaveInstanceState() =
-        ViewChildStateManager.onSaveInstanceState(this, super.onSaveInstanceState())
-
-    override fun onRestoreInstanceState(state: Parcelable?) =
-        super.onRestoreInstanceState(ViewChildStateManager.onRestoreInstanceState(this, state))
+    override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>?) =
+        ViewChildStateManager.dispatchRestoreInstanceState(this, container, ::dispatchThawSelfOnly)
 }
